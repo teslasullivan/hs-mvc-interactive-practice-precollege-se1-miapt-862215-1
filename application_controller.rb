6c17d40ac14292d78f53a25ba11b80edee112fbf
AlexandraWolf <alexandrawolf103@gmail.com>
2015-07-02 14:30:00 +0000

    mvp

--- application_controller.rb
+++ application_controller.rb
@@ -1,7 +1,13 @@
 require 'bundler'
 Bundler.require
+require 'rubygems' # not necessary with ruby 1.9 but included for completeness 
+require 'twilio-ruby' 
 
 class MyApp < Sinatra::Base
+  
+  # put your own credentials here 
+account_sid = 'AC06b0221d5531ccdac6be6fd3796bc6e3' 
+auth_token = '4884e02ce8278776089525f3af5d945f' 
 
   get '/' do
     erb :index
@@ -11,7 +17,18 @@ class MyApp < Sinatra::Base
   post '/results' do
     @number=params[:number]
     @message=params[:message]
-    erb :results
+ 
+ 
+    # set up a client to talk to the Twilio REST API 
+    @client = Twilio::REST::Client.new account_sid, auth_token 
+
+    @client.account.messages.create({
+    :from => +12692056881, 
+    :to => @number,
+    :body => @message 
+    })
+    
+   erb :results
+    
   end
-  
 end
