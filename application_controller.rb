f0ccc1b7740b596e75f7bd106b24f3da2ebee36d
teslasullivan <sullivan.tesla@gmail.com>
2015-07-02 14:05:59 +0000

    share

--- application_controller.rb
+++ application_controller.rb
@@ -15,3 +15,19 @@ class MyApp < Sinatra::Base
   end
   
 end
+
+require 'rubygems' # not necessary with ruby 1.9 but included for completeness 
+require 'twilio-ruby' 
+ 
+# put your own credentials here 
+account_sid = 'AC06b0221d5531ccdac6be6fd3796bc6e3' 
+auth_token = '[AuthToken]' 
+ 
+# set up a client to talk to the Twilio REST API 
+@client = Twilio::REST::Client.new account_sid, auth_token 
+ 
+@client.account.messages.create({
+	:from => '+12692056881', 
+	:to => '3054694168', 
+	:body => 'Hi ',  
+})
